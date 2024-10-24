6d39d159d476fc490e372f698dba5adbf9e60f35
Patrick Shriwise <pshriwise@gmail.com>
2024-10-24 13:01:04 -0500

    Changing extension to reduce confusion

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 curr_dir=$(dirname "$(realpath "$0")")
 echo "Generating toolbar file for directory: ${curr_dir}"
-sed "s|@DAGMC_TOOLBAR_LOCATION@|${curr_dir}|g" "${curr_dir}/toolbars/template.ttb" > toolbars/dagmc_toolbar.ttb
+sed "s|@DAGMC_TOOLBAR_LOCATION@|${curr_dir}|g" "${curr_dir}/toolbars/template.tmpl" > toolbars/dagmc_toolbar.ttb
